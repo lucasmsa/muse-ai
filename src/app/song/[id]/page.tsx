d7b382e36c651ef386a5c529fa3798c0581e36c9
Lucas Moreira <lucasmsea@outlook.com>
2024-08-12 18:50:42 -0300

    style: add z-indexed content wrapper to make sure the album image on the background does not overlay the cards

--- src/app/song/[id]/page.tsx
+++ src/app/song/[id]/page.tsx
@@ -70,40 +70,42 @@ export default function SongPage({ params: { id } }: SongPageProps) {
         alt="Song album cover image faded in the background"
         className="absolute right-0 mix-blend-overlay ml-auto max-tablet:max-w-0 max-tablet:max-h-0"
       />
-      <ContentWrapper>
-        <FadeAnimationWrapper>
-          <SongPlayerCard
-            title={song.title}
-            album={song.album}
-            artist={song.artist}
-            songPath={prependAssetsAudioPath(song.files.audio)}
-            favorite={{
-              onClick: () => setFavoriteSong(Number(song.id)),
-              isFavorited: favoritedSongsIds.has(Number(song.id)),
-            }}
-            coverArtPath={prependAssetsImagePath(song.files.coverArt)}
-          />
-          {relatedSongs?.length ? (
-            <div className="mt-24 flex flex-col max-tablet:mx-auto max-tablet:items-center">
-              <p className="text-fade-white text-base">Other albums</p>
-              <div className="flex flex-row w-full gap-8 flex-wrap mt-5 max-tablet:items-center max-tablet:justify-center">
-                {relatedSongs?.map((relatedSong) => (
-                  <Card
-                    id={relatedSong.id}
-                    key={relatedSong.id}
-                    title={relatedSong.song.title}
-                    artist={relatedSong.song.artist}
-                    albumTitle={relatedSong.song.album.title}
-                    coverArtPath={prependAssetsImagePath(
-                      relatedSong.song.files.coverArt,
-                    )}
-                  />
-                ))}
+      <div className="z-10">
+        <ContentWrapper>
+          <FadeAnimationWrapper>
+            <SongPlayerCard
+              title={song.title}
+              album={song.album}
+              artist={song.artist}
+              songPath={prependAssetsAudioPath(song.files.audio)}
+              favorite={{
+                onClick: () => setFavoriteSong(Number(song.id)),
+                isFavorited: favoritedSongsIds.has(Number(song.id)),
+              }}
+              coverArtPath={prependAssetsImagePath(song.files.coverArt)}
+            />
+            {relatedSongs?.length ? (
+              <div className="mt-24 flex flex-col max-tablet:mx-auto max-tablet:items-center">
+                <p className="text-fade-white text-base">Other albums</p>
+                <div className="flex flex-row w-full gap-8 flex-wrap mt-5 max-tablet:items-center max-tablet:justify-center">
+                  {relatedSongs?.map((relatedSong) => (
+                    <Card
+                      id={relatedSong.id}
+                      key={relatedSong.id}
+                      title={relatedSong.song.title}
+                      artist={relatedSong.song.artist}
+                      albumTitle={relatedSong.song.album.title}
+                      coverArtPath={prependAssetsImagePath(
+                        relatedSong.song.files.coverArt,
+                      )}
+                    />
+                  ))}
+                </div>
               </div>
-            </div>
-          ) : null}
-        </FadeAnimationWrapper>
-      </ContentWrapper>
+            ) : null}
+          </FadeAnimationWrapper>
+        </ContentWrapper>
+      </div>
     </>
   )
 }
